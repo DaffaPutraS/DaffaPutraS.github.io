eb4b13931788aee7fe55ae6ebdc49071054cfc0a
DaffaPutraS <daffap434@gmail.com>
2023-10-31 21:48:48 +0700

    Update Navigation

--- js/script.js
+++ js/script.js
@@ -11,13 +11,3 @@ window.onscroll = () =>{
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
 }
-
-var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-
-    });
-}
